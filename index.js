2b072be132e8fbf83583281baa1d4e350b42e459
Jami Hämäläinen <jami.hamala@gmail.com>
2024-12-31 14:11:29 +0200

    added userRouter

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express()
 
 const { PORT } = require('./util/config')
 const { connect } = require('./util/db')
+const userRouter = require('./routers/userRouter')
 
 app.use(express.json())
+app.use('/api/users', userRouter)
 
 const start = async () => {
   await connect()
