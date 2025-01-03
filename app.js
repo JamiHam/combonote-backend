57ee90d1bde9dcdb87096b57503ad0e599d42626
Jami Hämäläinen <jami.hamala@gmail.com>
2025-01-03 10:27:53 +0200

    created login_router.js

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express()
 
 const { PORT } = require('./utils/config')
 const { connect } = require('./utils/db')
-const userRouter = require('./routers/userRouter')
 const middleware = require('./utils/middleware')
+const userRouter = require('./routers/user_router')
+const loginRouter = require('./routers/login_router')
 
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
