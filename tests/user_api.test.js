dcfbf74b9d84b9b8209aa90931dc35d0dc37fbd9
Jami Hämäläinen <jami.hamala@gmail.com>
2025-01-06 15:05:37 +0200

    added login api tests

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,9 +1,9 @@
-const { test, beforeEach, describe } = require('node:test')
+const { test, beforeEach, describe, after, before } = require('node:test')
 const assert = require('node:assert')
-const supertest = require('supertest')
+const request = require('supertest')
 const app = require('../app')
-const api = supertest(app)
 
+const { sequelize } = require('../utils/db')
 const { User } = require('../models')
 
 const newUser = {
@@ -12,12 +12,12 @@ const newUser = {
 }
 
 beforeEach(async () => {
-  await User.truncate()
+  await User.truncate({ cascade: true })
 })
 
 describe('creating a new user', async () => {
   test('succeeds with valid data', async () => {
-    await api
+    await request(app)
       .post('/api/users')
       .send(newUser)
       .expect(201)
@@ -25,19 +25,50 @@ describe('creating a new user', async () => {
 
   test('fails if the username already exists', async () => {
     await User.create({ username: 'Alice', passwordHash: 'passwordHash' })
-
-    await api
+    
+    await request(app)
       .post('/api/users')
       .send(newUser)
       .expect(409)
   })
 })
 
-
 test('passwords are not stored as plaintext', async () => {
-  const createdUser = await api
+  const createdUser = await request(app)
     .post('/api/users')
     .send(newUser)
   
   assert.notEqual(newUser.password, createdUser.body.passwordHash)
+})
+
+describe('login', async () => {
+  beforeEach(beforeEach(async () => {
+    await request(app)
+      .post('/api/users')
+      .send(newUser)
+  }))
+
+  test('succeeds with correct username and password', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        username: 'Alice',
+        password: 'password123'
+      })
+      .expect(200)
+  })
+  
+  test('fails with incorrect password', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        username: 'Alice',
+        password: 'password321'
+      })
+      .expect(401)
+  })
+})
+
+after(async () => {
+  await sequelize.close()
 })
