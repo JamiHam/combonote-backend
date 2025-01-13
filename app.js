9b925a50669a1fa819d069628c7a503c242dfe90
Jami Hämäläinen <jami.hamala@gmail.com>
2025-01-13 14:38:32 +0200

    added index file for routers

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express()
 const { PORT } = require('./utils/config')
 const { connect } = require('./utils/db')
 const middleware = require('./utils/middleware')
-const userRouter = require('./routers/user_router')
-const loginRouter = require('./routers/login_router')
-const noteRouter = require('./routers/note_router')
-const tableRouter = require('./routers/table_router')
+const {
+  userRouter,
+  loginRouter,
+  noteRouter,
+  tableRouter,
+  columnRouter
+} = require('./routers')
 
 app.use(express.json())
 app.use(middleware.requestLogger)
@@ -16,6 +19,7 @@ app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/notes', noteRouter)
 app.use('/api/tables', tableRouter)
+app.use('/api/columns', columnRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ routers/index.js
@@ -0,0 +1,13 @@
+const userRouter = require('./user_router')
+const loginRouter = require('./login_router')
+const noteRouter = require('./note_router')
+const tableRouter = require('./table_router')
+const columnRouter = require('./column_router')
+
+module.exports = {
+  userRouter,
+  loginRouter,
+  noteRouter,
+  tableRouter,
+  columnRouter
+}
